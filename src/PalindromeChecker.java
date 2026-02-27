4c3894b0b2f527fd27dad7b3d0b244a9ac7abcb6
gd3312-beep <gruhith.1986@gmail.com>
2026-02-27 16:01:24 +0530

    UC6

--- src/PalindromeChecker.java
+++ src/PalindromeChecker.java
@@ -1,33 +1,41 @@
 import java.util.Scanner;
 import java.util.Stack;
+import java.util.Queue;
+import java.util.LinkedList;
 
 public class PalindromeChecker {
     public static void main(String[] args){
+
         Scanner scanner = new Scanner(System.in);
         Stack<Character> stack = new Stack<>();
+        Queue<Character> queue = new LinkedList<>();
 
         System.out.println("Palindrome Checker App");
-        System.out.println("UC5: Stack-Based Palindrome Checker");
+        System.out.println("UC6: Queue + Stack Based Palindrome Check");
 
         System.out.print("Enter a word: ");
         String word = scanner.nextLine();
 
         for(int i = 0; i < word.length(); i++) {
-            stack.push(word.charAt(i));
+            char ch = word.charAt(i);
+            stack.push(ch);      // LIFO
+            queue.add(ch);       // FIFO
         }
 
-        String reversed = "";
+        boolean isPalindrome = true;
 
         while(!stack.isEmpty()) {
-            reversed = reversed + stack.pop();
+            if(stack.pop() != queue.remove()) {
+                isPalindrome = false;
+                break;
+            }
         }
 
-        if(word.equals(reversed)) {
+        if(isPalindrome) {
             System.out.println(word + " is a Palindrome");
         } else {
             System.out.println(word + " is Not a Palindrome");
         }
-
 
         scanner.close();
     }
