1a88c57deb017f7202124580dcde7186e8c3cc66
gd3312-beep <gruhith.1986@gmail.com>
2026-02-27 15:53:52 +0530

    UC2

--- src/PalindromeChecker.java
+++ src/PalindromeChecker.java
@@ -1,37 +1,33 @@
 import java.util.Scanner;
+import java.util.Stack;
+
 public class PalindromeChecker {
     public static void main(String[] args){
-
         Scanner scanner = new Scanner(System.in);
+        Stack<Character> stack = new Stack<>();
 
         System.out.println("Palindrome Checker App");
-        System.out.println("UC4: Character Array Based Palindrome Check");
+        System.out.println("UC5: Stack-Based Palindrome Checker");
 
         System.out.print("Enter a word: ");
         String word = scanner.nextLine();
 
-        char[] characters = word.toCharArray();
-
-        int start = 0;
-        int end = characters.length - 1;
-        boolean isPalindrome = true;
-
-        while(start < end) {
-            if(characters[start] != characters[end]) {
-                isPalindrome = false;
-                break;
-            }
-            start++;
-            end--;
+        for(int i = 0; i < word.length(); i++) {
+            stack.push(word.charAt(i));
         }
 
-        if(isPalindrome) {
+        String reversed = "";
+
+        while(!stack.isEmpty()) {
+            reversed = reversed + stack.pop();
+        }
+
+        if(word.equals(reversed)) {
             System.out.println(word + " is a Palindrome");
         } else {
             System.out.println(word + " is Not a Palindrome");
         }
 
         scanner.close();
-
     }
 }
